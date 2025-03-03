bb5cdb8e8ae2eaa326a7ef2749c7a9546b171740
Adhanik <amitdhanik3@gmail.com>
2025-03-03 20:00:25 +0530

    Storing state file on S3 and lock using Dynamo DB

---
+++ Day 5 - Infra as code using TF/tf/backend/main.tf
@@ -0,0 +1,39 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a S3 Bucket
+
+resource "aws_s3_bucket" "example" {
+  bucket = "amit-eks-state-bucket-tf"
+  lifecycle {
+    prevent_destroy = false
+  }
+
+
+}
+
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "amit-terraform-state-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockId"
+
+  attribute {
+    name = "LockId"
+    type = "S"      #(Required) Attribute type. Valid values are S (string), N (number), B (binary).
+  }
+
+}
